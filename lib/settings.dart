322b574fffcd0350f96da8232ae5d425665aec18
stobertonf <stobertonf@gmail.com>
2022-06-19 18:42:03 -0300

    feat:(Settings): Creating The Settings Class

---
+++ lib/settings.dart
@@ -0,0 +1,7 @@
+import '../models/user.model.dart';
+
+class Settings {
+  static String apiUrl = "https://balta-eshop.azurewebsites.net/"; //url básica
+  static String theme = "light"; //tema padrão a ser carregado.
+  static UserModel user; //usuário padrão
+}
