710b4ede496ceaab09232997e732827aa5610bd5
stobertonf <stobertonf@gmail.com>
2022-07-07 22:01:18 -0300

    feat(Category-Card): Creating CategoryCard file.

---
+++ lib/ui/shared/widgets/category/category-card-widget.dart
@@ -0,0 +1,15 @@
+import '../../../../settings.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping/blocs/home.bloc.dart';
+import 'package:shopping/models/category-list-item.model.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
