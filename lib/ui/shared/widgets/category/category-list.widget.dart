773c6aee3d4b7d1bc6fea7ae4ce6b8c87d940583
stobertonf <stobertonf@gmail.com>
2022-07-07 22:32:15 -0300

    feat(Category-List): Creating CategoryList file.

--- lib/ui/shared/widgets/category/category-list.widget.dart
+++ lib/ui/shared/widgets/category/category-list.widget.dart
@@ -1,8 +1,40 @@
+import 'category-card-widget.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shopping/models/category-list-item.model.dart';
+import 'package:shopping/ui/shared/widgets/shared/loader.widget.dart';
+
 class CategoryList extends StatelessWidget {
-  const CategoryList({Key? key}) : super(key: key);
+  final List<CategoryListItemModel> categories;
+
+  CategoryList({@required this.categories});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 90,
+      child: Loader(
+        object: categories,
+        callback: list,
+      ),
+    );
+  }
+
+  Widget list() {
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        CategoryListItemModel item = categories[index];
+
+        return Padding(
+          padding: EdgeInsets.all(5),
+          child: CategoryCard(
+            item: item,
+          ),
+        );
+      },
+    );
   }
 }
