63c2706fcc2f3cb4dc414385f88ddc189fc16712
stobertonf <stobertonf@gmail.com>
2022-07-07 22:29:23 -0300

    feat(Category-List): Creating Loader file.

---
+++ lib/ui/shared/widgets/shared/loader.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+import 'package:shopping/ui/shared/widgets/shared/progress-indicator.widget.dart';
+
+class Loader extends StatelessWidget {
+  final object;
+  final Function callback;
+
+  Loader({@required this.object, @required this.callback});
+
+  @override
+  Widget build(BuildContext context) {
+    if (object == null)
+      return Center(
+        child: GenericProgressIndicator(),
+      );
+
+    if (object.length == 0)
+      return Center(
+        child: Text("Nenhum item encontrado"),
+      );
+
+    return callback();
+  }
+}
