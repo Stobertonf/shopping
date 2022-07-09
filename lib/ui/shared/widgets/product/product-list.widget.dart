766f5ff9a96f769a41f68391c8b1f780cedb601a
stobertonf <stobertonf@gmail.com>
2022-07-09 15:06:37 -0300

    feat(List-Products): Creating ListProducts file

---
+++ lib/ui/shared/widgets/product/product-list.widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class ProductList extends StatelessWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
