df9bd3b9149a4ab793e26fc84500012ec551bac9
stobertonf <stobertonf@gmail.com>
2022-07-09 18:48:13 -0300

    feat(Cart-Bloc): Adding calculateTotal

--- lib/blocs/cart.bloc.dart
+++ lib/blocs/cart.bloc.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/widgets.dart';
 import 'package:shopping/models/cart-item.model.dart';
 
-class CartBloc extends ChangeNotifier {}
+class CartBloc extends ChangeNotifier {
+  double total = 0;
+  var cart = new List<CartItemModel>();
+
+  get() {
+    return cart;
+  }
+
+  calculateTotal() {
+    total = 0;
+    cart.forEach((x) {
+      total += (x.price * x.quantity);
+    });
+    notifyListeners();
+  }
+}
