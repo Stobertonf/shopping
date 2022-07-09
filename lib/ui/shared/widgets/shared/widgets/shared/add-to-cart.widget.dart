7b25ef956cd6aacf9d6281f267b5a13ddc4f7cff
stobertonf <stobertonf@gmail.com>
2022-07-09 20:02:50 -0300

    feat(Cart): Creating Add To Cart File.

---
+++ lib/ui/shared/widgets/shared/widgets/shared/add-to-cart.widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddToCart extends StatelessWidget {
+  const AddToCart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
