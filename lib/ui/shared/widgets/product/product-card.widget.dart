7abd71bfec8d158faa6d95a8b0511b877093b03c
stobertonf <stobertonf@gmail.com>
2022-07-09 15:02:31 -0300

    feat(Product-Card): Finalizing the Product-Card.

--- lib/ui/shared/widgets/product/product-card.widget.dart
+++ lib/ui/shared/widgets/product/product-card.widget.dart
@@ -54,6 +54,45 @@ class ProductCard extends StatelessWidget {
           const SizedBox(
             height: 5,
           ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            child: Text(
+              item.brand,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 120,
+                  child: Text(
+                    "R\$ ${price.format(item.price)}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+                //AddToCart(item: item),
+              ],
+            ),
+          ),
         ],
       ),
     );
