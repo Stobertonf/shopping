c4be454a8ef7f979c5b6bcb670cbf8e5f0ca4d53
stobertonf <stobertonf@gmail.com>
2022-07-09 14:29:01 -0300

    feat(Product-Card): Creating ProductCard file.

---
+++ lib/ui/shared/widgets/product/product-card.widget.dart
@@ -0,0 +1,14 @@
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping/models/product-list-item.model.dart';
+import 'package:shopping/ui/android/pages/product.page.dart';
+import 'package:shopping/ui/shared/widgets/shared/add-to-cart.widget.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
