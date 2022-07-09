bf0a5ee0644a9b23fe1dbc513df147aa9bc52973
stobertonf <stobertonf@gmail.com>
2022-07-09 14:50:32 -0300

    feat(Product-Card): Creating Container To Display The Product.

--- lib/ui/shared/widgets/product/product-card.widget.dart
+++ lib/ui/shared/widgets/product/product-card.widget.dart
@@ -5,10 +5,37 @@ import 'package:shopping/ui/android/pages/product.page.dart';
 import 'package:shopping/ui/shared/widgets/shared/add-to-cart.widget.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({Key? key}) : super(key: key);
+  final ProductListItemModel item;
 
+  ProductCard({@required this.item});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final price = new NumberFormat("#,##0.00", "pt_BR");
+    return Container(
+      width: 240,
+      margin: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.03),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(5),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: 240,
+            height: 240,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.05),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(5),
+                topRight: Radius.circular(5),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
