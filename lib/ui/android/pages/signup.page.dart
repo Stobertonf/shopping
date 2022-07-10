224295f774545342eade273216458cca78f38d59
stobertonf <stobertonf@gmail.com>
2022-07-10 14:49:50 -0300

    feat(Unauthenticated): Creating SignupPage File.

---
+++ lib/ui/android/pages/signup.page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class SignupPage  extends StatefulWidget {
+  const SignupPage ({ Key? key }) : super(key: key);
+
+  @override
+  State<SignupPage > createState() => _SignupPage State();
+}
+
+class _SignupPage State extends State<SignupPage > {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/ui/shared/widgets/account/unauthenticated-user-card.widget.dart
+++ lib/ui/shared/widgets/account/unauthenticated-user-card.widget.dart
@@ -27,7 +27,7 @@ class UnauthenticatedUserCard extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => const LoginPage(),
+                builder: (context) => const SignupPage(),
               ),
             );
           },
