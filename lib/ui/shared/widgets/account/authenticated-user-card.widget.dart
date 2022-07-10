ad9ae26e0805140f9b7eea2aecb6db6b76346047
stobertonf <stobertonf@gmail.com>
2022-07-10 14:37:09 -0300

    feat(Authenticated): Creating AuthenticatedUserCard File.

---
+++ lib/ui/shared/widgets/account/authenticated-user-card.widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping/blocs/user.bloc.dart';
+
+class AuthenticatedUserCard extends StatelessWidget {
+  const AuthenticatedUserCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
