a0c0467376aa87c49616167942d41bd8b56995ab
stobertonf <stobertonf@gmail.com>
2022-07-10 14:32:17 -0300

    feat(User-Bloc): Finishing UserBloc.

--- lib/blocs/user.bloc.dart
+++ lib/blocs/user.bloc.dart
@@ -1,5 +1,64 @@
-import 'package:flutter/material.dart;
+import 'dart:convert';
+import '../settings.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping/models/user.model.dart';
+import 'package:shopping/models/create-user.model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shopping/models/authenticate-user.model.dart';
+import 'package:shopping/repositories/account.repository.dart';
 
-class UserBloc extends ChangeNotifier{
-  
+class UserBloc extends ChangeNotifier {
+  var user = new UserModel();
+
+  UserBloc() {
+    user = null;
+    loadUser();
+  }
+
+  Future<UserModel> authenticate(AuthenticateModel model) async {
+    try {
+      var prefs = await SharedPreferences.getInstance();
+      var repository = new AccountRepository(); //Chamando o repositório
+
+      var res = await repository.authenticate(model);
+
+      user = res;
+      await prefs.setString('user', jsonEncode(res));
+
+      return res;
+    } catch (ex) {
+      print(ex);
+      user = null;
+      return null;
+    }
+  }
+
+  Future<UserModel> create(CreateUserModel model) async {
+    try {
+      var repository = new AccountRepository(); //Chamando o repositório
+      var res = await repository.create(model);
+      return res;
+    } catch (ex) {
+      print(ex);
+      user = null;
+      return null;
+    }
+  }
+
+  logout() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user', null);
+    user = null;
+    notifyListeners();
+  }
+
+  Future loadUser() async {
+    var prefs = await SharedPreferences.getInstance();
+    var userData = prefs.getString('user');
+    if (userData != null) {
+      var res = UserModel.fromJson(jsonDecode(userData));
+      Settings.user = res;
+      user = res;
+    }
+  }
 }
