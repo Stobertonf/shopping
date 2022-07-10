131084aa8c10cd8f8ec9f552407913430a77c267
stobertonf <stobertonf@gmail.com>
2022-07-10 13:56:01 -0300

    feat(Cart-Item): Finishing CartPage

--- lib/ui/shared/widgets/cart/cart-item-widget.dart
+++ lib/ui/shared/widgets/cart/cart-item-widget.dart
@@ -5,10 +5,94 @@ import 'package:shopping/blocs/cart.bloc.dart';
 import 'package:shopping/models/cart-item.model.dart';
 
 class CartItem extends StatelessWidget {
-  const CartItem({Key? key}) : super(key: key);
+  final CartItemModel item;
+
+  CartItem({@required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final bloc = Provider.of<CartBloc>(context);
+    final price = new NumberFormat("#,##0.00", "pt_BR");
+
+    return Container(
+      height: 120,
+      margin: const EdgeInsets.all(5),
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 100,
+            margin: const EdgeInsets.all(
+              10,
+            ),
+            child: Image.network(
+              item.image,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20,
+              left: 10,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(item.title),
+                Text(
+                  "R\$ ${price.format(item.price)}",
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("R\$ ${price.format((item.price * item.quantity))}"),
+                Container(
+                  height: 30,
+                  width: 120,
+                  decoration: const BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 40,
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          child: Text("-"),
+                          onPressed: () {
+                            bloc.decrease(item);
+                          },
+                        ),
+                      ),
+                      Container(
+                        width: 40,
+                        alignment: Alignment.center,
+                        child: Text(item.quantity.toString()),
+                      ),
+                      Container(
+                        width: 40,
+                        alignment: Alignment.center,
+                        child: FlatButton(
+                          child: const Text("+"),
+                          onPressed: () {
+                            bloc.increase(item);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
