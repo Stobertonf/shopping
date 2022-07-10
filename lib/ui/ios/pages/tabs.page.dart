ec0ff5549e43b42efe0df2988aa60481e931e0ec
stobertonf <stobertonf@gmail.com>
2022-07-10 16:10:54 -0300

    feat(Tab-Page-IOS): Finishing Tab-Page-IOS.

--- lib/ui/ios/pages/tabs.page.dart
+++ lib/ui/ios/pages/tabs.page.dart
@@ -3,10 +3,42 @@ import 'package:flutter/material.dart';
 import 'package:shopping/ui/ios/pages/home.page.dart';
 
 class TabsPage extends StatelessWidget {
-  const TabsPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        backgroundColor: CupertinoColors.lightBackgroundGray,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            title: Text('Carrinho'),
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        return CupertinoTabView(
+          builder: (context) {
+            switch (index) {
+              case 0:
+                return HomePage();
+                break;
+              case 1:
+                return Container(
+                  color: Colors.red,
+                );
+                break;
+              default:
+                return Container(
+                  color: Colors.green,
+                );
+            }
+          },
+        );
+      },
+    );
   }
 }
