b8e1844d8cc47b352e522a5fbde52c100bcd661d
stobertonf <stobertonf@gmail.com>
2022-07-10 14:58:10 -0300

    feat(Account): Creating AccountPage File.

---
+++ lib/ui/android/pages/account.page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
