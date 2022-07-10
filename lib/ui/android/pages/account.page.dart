fce248674f2a22d526c1d6a8967b846f550e0df0
stobertonf <stobertonf@gmail.com>
2022-07-10 15:01:26 -0300

    feat(Authenticated): Finishing AccountPage.

--- lib/ui/android/pages/account.page.dart
+++ lib/ui/android/pages/account.page.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shopping/blocs/user.bloc.dart';
+import 'package:shopping/ui/android/pages/settings.page.dart';
+import 'package:shopping/ui/shared/widgets/account/authenticated-user-card.widget.dart';
+import 'package:shopping/ui/shared/widgets/account/unauthenticated-user-card.widget.dart';
 
 class AccountPage extends StatelessWidget {
-  const AccountPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var bloc = Provider.of<UserBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          FlatButton(
+            child: Icon(
+              Icons.settings,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SettingsPage(),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+      body: bloc.user == null
+          ? UnauthenticatedUserCard()
+          : AuthenticatedUserCard(),
+    );
   }
 }
