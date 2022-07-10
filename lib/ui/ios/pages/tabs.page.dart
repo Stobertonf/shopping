41dcf8ac4f26b7ec80c5ebf95e9d03554ea16964
stobertonf <stobertonf@gmail.com>
2022-07-10 16:09:38 -0300

    feat(Tab-Page-IOS): Creating Tab-Page-IOS File.

---
+++ lib/ui/ios/pages/tabs.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping/ui/ios/pages/home.page.dart';
+
+class TabsPage extends StatelessWidget {
+  const TabsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
