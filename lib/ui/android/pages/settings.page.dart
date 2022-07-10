d94f2583b9f0a372b3da3d2921bef55cc7b84bb3
stobertonf <stobertonf@gmail.com>
2022-07-10 15:56:37 -0300

    feat(Settings-Page): Finalizing settingspage File.

--- lib/ui/android/pages/settings.page.dart
+++ lib/ui/android/pages/settings.page.dart
@@ -1,11 +1,53 @@
+import '../../../settings.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shopping/blocs/theme.bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final ThemeBloc bloc = Provider.of<ThemeBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            "Tema atual: ${Settings.theme}",
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text("Light"),
+            onPressed: () {
+              save("light");
+              bloc.change('light');
+            },
+          ),
+          FlatButton(
+            child: Text("Dark"),
+            onPressed: () {
+              save("dark");
+              bloc.change('dark');
+            },
+          ),
+          FlatButton(
+            child: Text("Dark Yellow"),
+            onPressed: () {
+              save("dark-yellow");
+              bloc.change('dark-yellow');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  save(theme) async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('theme', theme);
   }
 }
