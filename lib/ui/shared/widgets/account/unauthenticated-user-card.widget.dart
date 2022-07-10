262441cbe7b928eb563f7bfcf563afa017476e25
stobertonf <stobertonf@gmail.com>
2022-07-10 14:42:54 -0300

    feat(Unauthenticated): Creating Unauthenticated File.

---
+++ lib/ui/shared/widgets/account/unauthenticated-user-card.widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UnauthenticatedUserCard extends StatelessWidget {
+  const UnauthenticatedUserCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
