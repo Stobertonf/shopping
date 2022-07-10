24734749d1f93f850af1ff9f08db6bd3aaf32fbc
stobertonf <stobertonf@gmail.com>
2022-07-10 15:19:35 -0300

    feat(Login-Page): Finishing LoginPage.

--- lib/ui/android/pages/login.page.dart
+++ lib/ui/android/pages/login.page.dart
@@ -1,10 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping/blocs/user.bloc.dart';
+import 'package:shopping/models/authenticate-user.model.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  var username = '';
+  var password = '';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(
+          20,
+        ),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Usuário",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Usuário Inválido';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  username = val;
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Senha Inválida';
+                  }
+                  return null;
+                },
+                onSaved: (val) {
+                  password = val;
+                },
+              ),
+              FlatButton(
+                child: Text("Entrar"),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    authenticate(context);
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  authenticate(BuildContext context) async {
+    var bloc = Provider.of<UserBloc>(context);
+
+    var user = await bloc.authenticate(
+      new AuthenticateModel(
+        username: username,
+        password: password,
+      ),
+    );
+
+    if (user != null) {
+      Navigator.pop(context);
+      return;
+    }
+
+    final snackBar = const SnackBar(
+      content: const Text(
+        'Usuário ou senha invalidos',
+      ),
+    );
+    _scaffoldKey.currentState.showSnackBar(snackBar);
   }
 }
