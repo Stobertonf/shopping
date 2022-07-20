284d79e9896bfd0012a941f6801da9b96fbb7969
stobertonf <stobertonf@gmail.com>
2022-07-20 18:43:39 -0300

    feat(User-Card): Creating Use-rCard File.

---
+++ lib/ui/ios/widgets/user-card.widget.dart.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserCard extends StatelessWidget {
+  const UserCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
